74c748c46a6d19e49684ad3ec370969a6a792b6e
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-10-26 19:30:25 +0530

    Create TwoTable.java

---
+++ TwoTable.java
@@ -0,0 +1,16 @@
+
+public class TwoTable {
+
+	public static void main(String[] args) {
+		int a=2;
+		
+		while(a<=100) {
+			if(a%2==0) {
+				System.out.println(a);
+			}
+			a++;
+		}
+
+	}
+
+}
