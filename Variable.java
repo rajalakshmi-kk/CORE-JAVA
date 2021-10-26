a28b4f91328667a08c2d3fd2981a8f618d07bef5
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-10-26 19:31:06 +0530

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,15 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myName = "Rajalakshmi K K";
+		String myJob = "Programmer";
+		int age = 21;
+		System.out.println(myName);
+		System.out.println(myJob);
+		System.out.println(age);
+		
+	}
+
+}
