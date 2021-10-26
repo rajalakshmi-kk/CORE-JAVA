7910c05132b3d6b0d0bdb5d4f3f868077d55e6c0
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-10-26 19:11:03 +0530

    Create OddNumbers.java

---
+++ OddNumbers.java
@@ -0,0 +1,11 @@
+
+public class OddNumbers {
+	public static void main(String[] args) {
+		int i;
+		System.out.println("the odd numbers are :");
+		for(i=1;i<=100;i+=2) {
+			System.out.println(i);
+		}
+
+}
+}
