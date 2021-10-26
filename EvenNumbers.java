6bd3afdf5b3ca885a67f38dec4171a8bdee94b2e
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-10-26 18:55:20 +0530

    Create EvenNumbers.java

---
+++ EvenNumbers.java
@@ -0,0 +1,15 @@
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		int i;
+		System.out.println("the even numbers are :");
+		for(i=1;i<=100;i++) {
+			if(i%2==0) {
+				
+				System.out.println(i +" ");
+				}
+		}
+	}
+
+}
