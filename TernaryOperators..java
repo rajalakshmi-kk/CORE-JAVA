6c51ff99f6b09e506f77535f6b6533e55b3269a4
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-10-26 19:20:12 +0530

    Create TernaryOperators..java

---
+++ TernaryOperators..java
@@ -0,0 +1,12 @@
+public class TernaryOperators {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=7;
+int j=0;
+
+j=i>9?0:1;
+System.out.println("After applying ternary operator:"+" "+j);
+
+	}
+
+}
